894acf229e754c36ff926bbb6793ff7750b9562c
Xavier <xavier.claerhoudt@gmail.com>
2018-03-22 18:08:55 +0100

    (live): live visualisation done

--- Code/Visualization/Visualisation/Assets/DroneScript.cs
+++ Code/Visualization/Visualisation/Assets/DroneScript.cs
@@ -1,23 +1,38 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Net;
+using System.Text;
+using UnityEngine;
 using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 public class DroneScript : MonoBehaviour {
     public MqttClient client;
+    public static IPAddress ip = IPAddress.Parse("157.193.214.115");
+    static string position = "0,0,0";
+
+    // message handling code
+    static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+    {
+        position = Encoding.UTF8.GetString(e.Message);
+    }
+
     // Use this for initialization
     void Start() {
         // port is default port dus moet niet specified worden
         // 157.193.214.115 port 1883
-        client = new MqttClient("157.193.214.115");
-        client.Subscribe(new string[] { "vopposition" }, new byte[] { 0 });
+        client = new MqttClient(ip);
+        //register to message received
         client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
-         // dan loop starten (loop_start)
-         //
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Vector3 coords = GetCoordinates("position.txt");
+        client.Connect(Guid.NewGuid().ToString());
+        // subscribe to vopposition topic
+        client.Subscribe(new string[] { "vopposition" }, new byte[] { 0 });
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 coords = GetCoords(position);
         Vector3 goodcoords = TransformCoordinates(coords);
+        goodcoords.y = 1;
         transform.position = goodcoords;
 	}
 
@@ -31,7 +46,8 @@ public class DroneScript : MonoBehaviour {
         return coords;
     }
 
-    Vector3 GetCoordinates (string fileloc)
+    //get coordinates from a file
+    Vector3 GetCoordsFromFile (string fileloc)
     {
         string position = System.IO.File.ReadAllText(fileloc);
         string[] test = position.Split(',');
@@ -42,4 +58,16 @@ public class DroneScript : MonoBehaviour {
         return coords;
     }
 
+    Vector3 GetCoords (string position)
+    {
+        string[] coord = position.Split(',');
+        coord[0] = coord[0].Replace('.', ',');
+        coord[1] = coord[1].Replace('.', ',');
+        coord[2] = coord[2].Replace('.', ',');
+        float x = float.Parse(coord[0]);
+        float y = float.Parse(coord[1]);
+        long z = long.Parse(coord[2]);
+        Vector3 coords = new Vector3(x, y, z);
+        return coords;
+    }
 }
