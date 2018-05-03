6b3306b1a6ab6470630ba5315c49ebae8031ee68
xclaerho <Xavier.Claerhoudt@UGent.be>
2018-05-03 17:28:36 +0200

    multiple drones supported

--- Code/Visualization/Visualisation/Assets/Scripts/DroneScript.cs
+++ Code/Visualization/Visualisation/Assets/Scripts/DroneScript.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Net;
 using System.Text;
 using UnityEngine;
@@ -8,51 +10,69 @@ using uPLibrary.Networking.M2Mqtt.Messages;
 public class DroneScript : MonoBehaviour {
     public MqttClient client;
     // ip-address of mqtt server
-    public static IPAddress ip = IPAddress.Parse("157.193.214.115");
+    public static IPAddress ip;
+    public GameObject Drone;
+    static Dictionary<int, GameObject> droneDictionary = new Dictionary<int, GameObject>();
     // Topics to subscribe to
     static string positionTopic = "vopposition";
     static string heightTopic = "vopheight";
     static string orientationTopic = "vopeulerangles";
     // Placeholder for updated position, height & orientation
-    static string position = "29000,1000";
-    static string height = "1000";
-    static string orientation = "0,0,0";
+    static Dictionary<int, string> positions = new Dictionary<int, string>();
+    static Dictionary<int, string> heights = new Dictionary<int, string>();
+    static Dictionary<int, string> orientations = new Dictionary<int, string>();
+    // Scale of the drone
+    static int dronescale = 100;
 
     /** Message handling code */
     static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
     {
-        if (e.Topic.Equals(positionTopic))
+        List<int> ids = new List<int>(droneDictionary.Keys);
+        foreach(int id in ids)
         {
-            position = Encoding.UTF8.GetString(e.Message);
-        }
-        else if (e.Topic.Equals(heightTopic))
-        {
-            height = Encoding.UTF8.GetString(e.Message);
-        }
-        else if (e.Topic.Equals(orientationTopic))
-        {
-            orientation = Encoding.UTF8.GetString(e.Message);
+            if (e.Topic.Equals(positionTopic+id.ToString()))
+            {
+                positions[id] = Encoding.UTF8.GetString(e.Message);
+            }
+            else if (e.Topic.Equals(heightTopic+id.ToString()))
+            {
+                heights[id] = Encoding.UTF8.GetString(e.Message);
+            }
+            else if (e.Topic.Equals(orientationTopic+id.ToString()))
+            {
+                orientations[id] = Encoding.UTF8.GetString(e.Message);
+            }
         }
     }
 
     /** Use this for initialization */
     void Start() {
+        // Read config file for setup
+        Setup();
         // Port is default port so doesnt have to be specified
         // 157.193.214.115 port 1883
         client = new MqttClient(ip);
         // Register to message received
         client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
         client.Connect(Guid.NewGuid().ToString());
-        // Subscribe to vopposition, vopheight and voporientation topic
-        client.Subscribe(new string[] { positionTopic, heightTopic, orientationTopic }, new byte[] { 0, 0, 0 });
+        // Subscribe to vopposition, vopheight and voporientation topic for all id's
+        List<int> ids = new List<int>(droneDictionary.Keys);
+        foreach (int id in ids)
+        {
+            client.Subscribe(new string[] { positionTopic+id.ToString(), heightTopic+id.ToString(), orientationTopic+id.ToString() }, new byte[] { 0, 0, 0 });
+        }
     }
 
     /** Update is called once per frame */
     void Update () {
-        // Update location
-		transform.position = TransformCoordinates (GetCoords ());
-        // Update orientation
-		transform.rotation = GetUnityOrientation();
+        List<int> ids = new List<int>(droneDictionary.Keys);
+        foreach (int id in ids)
+        {
+            // Update location
+            droneDictionary[id].transform.position = TransformCoordinates(GetCoords(id));
+            // Update orientation
+            droneDictionary[id].transform.rotation = GetUnityOrientation(id);
+        }
 	}
 
     /** Transforms coordinates from the MQTT coordinate system to unity coordinate system */
@@ -68,28 +88,80 @@ public class DroneScript : MonoBehaviour {
     /** Gets coordinates from a string where they are as follows: "x,y,z"
      * and each decimal is indicated by a dot
      */
-    Vector3 GetCoords ()
+    Vector3 GetCoords (int id)
     {
         // Coords are seperated by a comma and decimals are indicated by a dot
-        string[] coord = position.Split(',');
+        string[] coord = positions[id].Split(',');
         // Unity uses comma's instead of dot's for decimals
         float x = float.Parse(coord[0].Replace('.', ','));
         float y = float.Parse(coord[1].Replace('.', ','));
-        float z = float.Parse(height.Replace('.',','));
+        float z = float.Parse(heights[id].Replace('.',','));
 
 		return new Vector3(x, y, z);
     }
 
     /** Takes angles sent by drone and changes the rotation of the drone in unity*/
-    Quaternion GetUnityOrientation()
+    Quaternion GetUnityOrientation(int id)
     {
 		// Angles are seperated by a comma and decimals are indicated by a dot
-        string[] angles = orientation.Split(',');
+        string[] angles = orientations[id].Split(',');
 		// Unity uses comma's instead of dot's for decimals
         float heading = float.Parse(angles[0].Replace('.', ','))-90;
         float roll = float.Parse(angles[1].Replace('.', ','));
         float pitch = -float.Parse(angles[2].Replace('.', ','));
 
 		return Quaternion.Euler(roll, heading, pitch);
+    }
+
+    /** Reads config file and sets all variables accordingly */
+    void Setup()
+    {
+        string[] config = System.IO.File.ReadAllLines(Application.dataPath + "/Config/Config.json");
+        for(int i=0; i<config.Length; i++)
+        {
+            if (config[i].Contains("ip"))
+            {
+                string ipaddress = config[i].Split(':')[1].Split(',')[0].Split('"')[1];
+                ip = IPAddress.Parse(ipaddress);
+
+            }
+            else if (config[i].Contains("dronescale"))
+            {
+                dronescale = Int32.Parse(config[i].Split(':')[1].Split('"')[1]);
+            }
+            else if (config[i].Contains("Drones"))
+            {
+                i++;
+                // Instantiate all drones
+                int id = 0;
+                while (!config[i].Contains("]"))
+                {
+                    if (config[i].Contains("id"))
+                    {
+                        GameObject drone = Instantiate(Drone, new Vector3(0, i * 1000, 0), Quaternion.identity);
+                        drone.transform.localScale = new Vector3(dronescale, dronescale, dronescale);
+                        id = Int32.Parse(config[i].Split(':')[1].Split(',')[0].Split('"')[1]);
+                        droneDictionary.Add(id, drone);
+                        positions.Add(id, "0,0");
+                        heights.Add(id,(id * 1000).ToString());
+                        orientations.Add(id, "0,0,0");
+                    }
+                    else if (config[i].Contains("color"))
+                    {
+                        string color = config[i].Split(':')[1].Split(',')[0].Split('"')[1].Trim();
+                        Color desiredColor = Color.black;
+                        ColorUtility.TryParseHtmlString(color, out desiredColor);
+
+                        // look for drone
+                        GameObject drone = null;
+                        droneDictionary.TryGetValue(id, out drone);
+                        // set it's color
+                        Renderer rend = drone.GetComponentInChildren<Renderer>();
+                        rend.material.SetColor("_Color", desiredColor);
+                    }
+                    i++;
+                }
+            }
+        }
     }
 }
